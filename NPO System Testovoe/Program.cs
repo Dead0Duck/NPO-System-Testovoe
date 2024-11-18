d87172879b8c0b2d365cfb3b883a148942c9a8dd
DeadDuck <git@dead0duck.ru>
2024-11-18 11:55:39 +0700

    Добавьте файлы проекта.

---
+++ NPO System Testovoe/Program.cs
@@ -0,0 +1,25 @@
+﻿using NPO_System_Testovoe;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string? url = args.Length >= 1 ? args[0] : null;
+        while (url == null)
+        {
+            Console.WriteLine("Введите путь/ссылку для получения информации о тендере:");
+            url = Console.ReadLine();
+        }
+
+        Console.WriteLine("Поиск информации по пути/ссылке: " + url);
+
+        if (url.StartsWith("http"))
+        {
+            var uri = new Uri(url);
+            Console.WriteLine(uri.AbsolutePath);
+        }
+
+        // var test = new Tender(12, "privte", 12, "RUB", DateTime.Now, "HI", null);
+        // Console.WriteLine(test.tenderName);
+    }
+}
---
+++ NPO System Testovoe/Tender.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPO_System_Testovoe
+{
+    internal class Tender
+    {
+        public readonly uint govRuId;
+        public readonly string tenderName;
+        public readonly double cost;
+        public readonly string costCurrency;
+        public readonly DateTime datePublic;
+        public readonly string customerName;
+        public readonly uint? customerInn;
+
+        public Tender(uint govRuId, string tenderName, double cost, string costCurrency, DateTime datePublic, string customerName, uint? customerInn)
+        {
+            this.govRuId=govRuId;
+            this.tenderName=tenderName;
+            this.cost=cost;
+            this.costCurrency=costCurrency;
+            this.datePublic=datePublic;
+            this.customerName=customerName;
+            this.customerInn=customerInn;
+        }
+    }
+}
